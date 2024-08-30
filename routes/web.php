65938a04c084df688a630ea3405563e899ced6a6
Fabricio <fabricio.eduardo2004@gmail.com>
2024-08-30 20:46:32 -0300

    attt

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+php artisan migrate:fresh
 
 
-Route::get('/', function () {
-    return view("principal.index");
-});
-
-
-
-Route::get('produtos/', function () {
-    return view('produto.lista');
-});
-
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos',[ProdutosController::class, 'index']);
+Route::get('/produtos/criar',[ProdutosController::class, 'novo']);
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
